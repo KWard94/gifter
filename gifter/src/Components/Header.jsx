7477e6f03d5c61b7a8b3ff46227c292fed3a980a
KWard <pcward.k@gmail.com>
2021-03-03 15:34:57 -0700

    changed fonts

--- gifter/src/Components/Header.jsx
+++ gifter/src/Components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <div className="header">
       <div className="header-words">
-        <h1 id="title">Gifter</h1>
+        <h1 id="title">~ Gifter ~</h1>
         <h5 id="subtitle">The Perfect Gift Finder</h5>
       </div>
       <Navigation />
